452db5dde20d4d64523c6fd691ef9e38d9fb00d8
ash8898 <ash889823@gmail.com>
2018-10-30 09:50:23 +0900

    create django app accounts

--- codev/settings.py
+++ codev/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'codev.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'accounts/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
